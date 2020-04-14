984df3bd2e8c7fd055171ae271fba09927a815fd
vking34 <lzzvkingzzl@gmail.com>
2020-04-14 16:24:44 +0700

    fix(redundant): remove redundant libs

--- src/routes/index.js
+++ src/routes/index.js
@@ -15,12 +15,12 @@ const App = ({match}) => (
     <Switch>
       <Route path={`${match.url}main`} component={Main}/>
       <Route path={`${match.url}components`} component={Components}/>
-      <Route path={`${match.url}custom-views`} component={CustomViews}/>
+      {/*<Route path={`${match.url}custom-views`} component={CustomViews}/>*/}
       {/*<Route path={`${match.url}extensions`} component={Extensions}/>*/}
       <Route path={`${match.url}extra-components`} component={ExtraComponents}/>
       <Route path={`${match.url}in-built-apps`} component={InBuiltApps}/>
       <Route path={`${match.url}social-apps`} component={SocialApps}/>
-      <Route path={`${match.url}documents`} component={Documents}/>
+      {/*<Route path={`${match.url}documents`} component={Documents}/>*/}
     </Switch>
   </div>
 );
