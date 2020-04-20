481c4d37d0fbc4e85b406ccc4a839fb57bd7ae52
vking34 <lzzvkingzzl@gmail.com>
2020-04-20 15:58:24 +0700

    feat(customers): add form to create/update user

---
+++ src/constants/Regex.js
@@ -0,0 +1,3 @@
+export const PHONE_REGEX = "^(0|\\+84)(\\s|\\.)?((3[2-9])|(5[689])|(7[06-9])|(8[1-689])|(9[0-46-9]))(\\d)(\\s|\\.)?(\\d{3})(\\s|\\.)?(\\d{3})$";
+export const USERNAME_REGEX = "^.{8,}$";
+// export const PASSWORD_REGEX =
--- src/routes/main/Customers/CustomerModal.js
+++ src/routes/main/Customers/CustomerModal.js
@@ -1,13 +1,18 @@
 import React from "react";
-import {Avatar, Input, Modal} from "antd";
+import {Avatar, Input, Modal, Form} from "antd";
 import IntlMessages from "../../../util/IntlMessages";
 import {connect} from "react-redux";
 import {logout} from "../../../util/Debug";
+import {PHONE_REGEX, USERNAME_REGEX} from "../../../constants/Regex";
 
 
 const thumb = require('assets/images/placeholder.jpg');
+const FormItem = Form.Item;
 
-class CustomerModal extends React.Component{
+
+class CustomerModal extends React.Component {
+
+  // form = Form.useForm();
 
   constructor(props) {
     super(props);
@@ -29,6 +34,102 @@ class CustomerModal extends React.Component{
     // logout('customer: ', this.props.selectedCustomers[0]);
     // logout(this.props.customerList[this.props.selectedCustomers[0]]);
     // logout({key, name, phone, email, username, password});
+    // return (
+    //   <Modal
+    //     title={edit ?
+    //       <IntlMessages id="actions.edit.customer"/> :
+    //       <IntlMessages id="actions.add.customer"/>
+    //     }
+    //     toggle={onUserClose}
+    //     visible={open}
+    //     closable={false}
+    //     onOk={() => {
+    //       if (name === ''){   // if name is empty, then does not close modal
+    //         return;
+    //       }
+    //
+    //       onUserClose();
+    //       logout(this.state);
+    //       onSaveUser(
+    //         edit,
+    //         {
+    //           'name': name,
+    //           'email': email,
+    //           'phone': phone,
+    //           'username': username,
+    //           'password': password
+    //         }
+    //       );
+    //       this.setState({
+    //         'key': key + 1,
+    //         'name': '',
+    //         'email': '',
+    //         'phone': '',
+    //         'username': ''
+    //       });
+    //     }}
+    //     onCancel={onUserClose}
+    //   >
+    //     <div className="gx-modal-box-row">
+    //       <div className="gx-modal-box-avatar">
+    //         <Avatar size="large" src={thumb}/>
+    //       </div>
+    //
+    //       <div className="gx-modal-box-form-item">
+    //
+    //         <div className="gx-form-group">
+    //           <Input
+    //             placeholder="Username"
+    //             onChange={(event) => this.setState({username: event.target.value})}
+    //             value={username}
+    //             margin="normal"/>
+    //         </div>
+    //
+    //         {
+    //           !edit ?
+    //             <div className="gx-form-group">
+    //               <Input.Password
+    //                 placeholder="Password"
+    //                 onChange={(event) => this.setState({password: event.target.value})}
+    //                 value={password}
+    //                 margin="normal"/>
+    //             </div> : null
+    //         }
+    //
+    //         <div className="gx-form-group">
+    //           <Input
+    //             required
+    //             placeholder="Name"
+    //             onChange={(event) => this.setState({name: event.target.value})}
+    //             defaultValue={name}
+    //             margin="none"/>
+    //         </div>
+    //
+    //         <div className="gx-form-group">
+    //           <Input
+    //             placeholder="Email"
+    //             onChange={(event) => this.setState({email: event.target.value})}
+    //             defaultValue={email}
+    //             margin="normal"/>
+    //         </div>
+    //
+    //         <div className="gx-form-group">
+    //           <Input
+    //             placeholder="Phone"
+    //             onChange={(event) => this.setState({phone: event.target.value})}
+    //             defaultValue={phone}
+    //             margin="normal"
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </Modal>
+    // );
+
+    // const {getFieldDecorator} = form;
+
+    const {getFieldDecorator} = this.props.form;
+
     return (
       <Modal
         title={edit ?
@@ -39,97 +140,130 @@ class CustomerModal extends React.Component{
         visible={open}
         closable={false}
         onOk={() => {
-          if (name === ''){   // if name is empty, then does not close modal
-            return;
+          this.props.form.validateFields()
+            .then(value => {
+              logout(value);
+              this.props.form.resetFields();
+              onUserClose();
+              logout(this.state);
+              onSaveUser(
+                edit,
+                value
+              );
+              this.setState({
+                'key': key + 1,
+                'name': '',
+                'email': '',
+                'phone': '',
+                'username': ''
+              });
+            })
+        }}
+
+        onCancel={() => {
+          this.props.form.resetFields();
+          onUserClose();
+        }}
+      >
+        <Form>
+          <FormItem
+            name="username"
+            label="Username"
+          >
+            {getFieldDecorator('username', {
+              initialValue: username,
+              rules: [
+                {
+                  required: true,
+                  pattern: new RegExp(USERNAME_REGEX),   // no need to define type of validation. In docs of antd, it said there a "type" field, but it does not work.
+                  message: 'Invalid Username!'
+                }, {
+                  required: true, message: 'Please input your username!',
+                },]
+            })(
+              <Input id="username" disabled={edit}/>
+            )}
+          </FormItem>
+
+          { edit ? null :
+            <FormItem
+              name="password"
+              label="Password"
+            >
+              {getFieldDecorator('password', {
+                rules: [{
+                  required: true, message: 'Please input your password!',
+                }, {
+                  required: true,
+                  min: 6,
+                  message: 'Password at least 6 characters!'
+                }],
+              })(
+                <Input.Password id="password"/>
+              )}
+            </FormItem>
           }
 
-          onUserClose();
-          logout(this.state);
-          onSaveUser(
-            edit,
-            {
-              'name': name,
-              'email': email,
-              'phone': phone,
-              'username': username,
-              'password': password
-            }
-          );
-          this.setState({
-            'key': key + 1,
-            'name': '',
-            'email': '',
-            'phone': '',
-            'username': ''
-          });
-        }}
-        onCancel={onUserClose}
-      >
-        <div className="gx-modal-box-row">
-          <div className="gx-modal-box-avatar">
-            <Avatar size="large" src={thumb}/>
-          </div>
+          <FormItem
+            name="name"
+            label="Name"
+          >
+            {getFieldDecorator('name', {
+              initialValue: name,
+              rules: [{
+                required: true, message: 'Please input the name',
+              }]
+            })(
+              <Input id="name"/>
+            )}
+          </FormItem>
 
-          <div className="gx-modal-box-form-item">
+          <FormItem
+            name="email"
+            label="Email"
+          >
+            {getFieldDecorator('email', {
+              initialValue: email,
+              rules: [{
+                type: 'email', message: 'The input is not valid E-mail!',
+              }, {
+                required: true, message: 'Please input your E-mail!',
+              }],
+            })(
+              <Input id="email1"/>
+            )}
+          </FormItem>
 
-            <div className="gx-form-group">
-              <Input
-                placeholder="Username"
-                onChange={(event) => this.setState({username: event.target.value})}
-                value={username}
-                margin="normal"/>
-            </div>
+          <FormItem
+            name="phone"
+            label="Phone Number"
+          >
+            {getFieldDecorator('phone', {
+              initialValue: phone,
+              rules: [{
+                require: true,
+                message: 'Invalid phone number!',
+                pattern: new RegExp(PHONE_REGEX)
+              }, {
+                required: true, message: 'Please input your phone number!',
+              }],
+            })(
+              <Input id="phone"/>
+            )}
+          </FormItem>
 
-            {
-              !edit ?
-                <div className="gx-form-group">
-                  <Input.Password
-                    placeholder="Password"
-                    onChange={(event) => this.setState({password: event.target.value})}
-                    value={password}
-                    margin="normal"/>
-                </div> : null
-            }
-
-            <div className="gx-form-group">
-              <Input
-                required
-                placeholder="Name"
-                onChange={(event) => this.setState({name: event.target.value})}
-                defaultValue={name}
-                margin="none"/>
-            </div>
-
-            <div className="gx-form-group">
-              <Input
-                placeholder="Email"
-                onChange={(event) => this.setState({email: event.target.value})}
-                defaultValue={email}
-                margin="normal"/>
-            </div>
-
-            <div className="gx-form-group">
-              <Input
-                placeholder="Phone"
-                onChange={(event) => this.setState({phone: event.target.value})}
-                defaultValue={phone}
-                margin="normal"
-              />
-            </div>
-          </div>
-        </div>
+        </Form>
       </Modal>
     );
   }
 }
+
+
+const CustomerFormModal = Form.create()(CustomerModal);
 
 const mapStateToProps = ({customers}) => {
   const {customerList, selectedCustomers, edit} = customers;
   return {customerList, selectedCustomers, edit};
 };
 
-
-
-export default connect(mapStateToProps, {
-
-})(CustomerModal);
+export default connect(mapStateToProps, {})(CustomerFormModal);
